c3affda7fab478da1b8c9c5bf017a1ca85a92b99
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2021-04-20 19:22:08 +0530

    Create first_occurrence.cpp

---
+++ Recursion/first_occurrence.cpp
@@ -0,0 +1,21 @@
+int firstIndex(int arr[], int idx, int x, int n){
+    if(idx==n)
+        return -1;
+    if(arr[idx]==x)
+        return idx;
+    int ans = firstIndex(arr,idx+1,x,n);
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int d;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cin >> d;
+    int p = firstIndex(arr, 0, d, n);
+    cout << p << endl;
+}
