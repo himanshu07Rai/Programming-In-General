5746ecce62b86286bbede7eaca2fc45293cdee6d
Himanshu Rai <64080063+himanshu07Rai@users.noreply.github.com>
2021-05-09 14:21:58 +0530

    Update dfs.cpp

--- Graphs/dfs.cpp
+++ Graphs/dfs.cpp
@@ -1,3 +1,55 @@
+/*
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+*/
+
+
+class Solution {
+public:
+    bool isNeighbour(int i,int j,int n,int m)
+    {
+        return i>=0 && i<n && j>=0 && j<m;
+    }
+    void dfs(vector<vector<char>>& grid,vector<vector<bool>> &visited, int i,int j,int n,int m,vector<vector<int>>&dir)
+    {
+        visited[i][j]=true;
+        for(int k=0;k<4;k++)
+        {
+            int ni=i+dir[k][0],nj=j+dir[k][1];
+            if(isNeighbour(ni,nj,n,m) && grid[ni][nj]=='1' && !visited[ni][nj])
+                dfs(grid,visited,ni,nj,n,m,dir);
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size(),m = grid[0].size();
+        if(!n)
+            return 0;
+        int count = 0;
+        vector<vector<bool>> visited(n,vector<bool>(m));
+        vector<vector<int>> dir = {{0,-1},{-1,0},{0,1},{1,0}};
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (grid[i][j] == '1' && !visited[i][j])
+                {
+                    count++;
+                    dfs(grid, visited, i, j,n,m,dir);
+                }
+            }
+        }
+        
+
+    return count;
+    }
+};
+
+
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
