5b65e5982b1e26f4d57ecd993d7098aaa5247335
Himanshu Rai <raihimanshu22345@gmail.com>
2021-11-07 17:15:47 +0530

    bubble sort

--- sorting/bubble.cpp
+++ sorting/bubble.cpp
@@ -1,27 +1,38 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-void bubble(int arr[], int n)
+void bubbleSort(int array[], int n)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n - 1; i++)
     {
-        for (int j = 0; j < n - i; j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
-            if (arr[j] > arr[j + 1])
+            if (array[j] > array[j + 1])
             {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
             }
         }
     }
 }
+void printArray(int array[], int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        cout << "  " << array[i];
+    }
+    cout << "\n";
+}
 
 int main()
 {
-    int arr[] = {7,8,9,1};
-    bubble(arr, 4);
-    for (int i = 0; i < 4; i++)
-        cout << arr[i] << "  ";
-    cout << endl;
+    int data[] = {5,4, 8, 6, 2};
+
+    int size = sizeof(data) / sizeof(data[0]);
+
+    bubbleSort(data, size);
+
+    cout << "Sorted Array in Ascending Order:\n";
+    printArray(data, size);
 }
