8e19ee12c2f86979c8c238120fa3dee5bbfa08f9
Himanshu Rai <raihimanshu22345@gmail.com>
2021-11-21 11:23:49 +0530

    lcs

---
+++ DP/lcs/lcs.cpp
@@ -0,0 +1,18 @@
+int lcs(int x, int y, string s1, string s2)
+{
+    // your code here
+    vector<vector<int>> dp(x + 1, vector<int>(y + 1, -1));
+    for (int i = 0; i <= x; i++)
+    {
+        for (int j = 0; j <= y; j++)
+        {
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+            else if (s1[i - 1] == s2[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    return dp[x][y];
+}
